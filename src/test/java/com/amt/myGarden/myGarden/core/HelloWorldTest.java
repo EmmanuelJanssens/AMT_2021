c40bef960f8ac61165d86a7f0c1205bb00e1ba3a
LordTT <ferraricorneloup.teo@gmail.com>
2021-10-04 11:36:59 +0200

    mock test failure3

---
+++ src/test/java/com/amt/myGarden/myGarden/core/HelloWorldTest.java
@@ -0,0 +1,24 @@
+package com.amt.myGarden.myGarden.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+        int testingNumber;
+
+        @BeforeEach
+        void setup(){
+            testingNumber = 1;
+        }
+
+        @Test
+        void test(){
+            //testingNumber++;
+            Assertions.assertEquals(2 ,testingNumber);
+        }
+
+}
