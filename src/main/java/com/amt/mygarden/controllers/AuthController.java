9e9b3278dd21d46eeabb7d43beaf38815901dddd
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2021-12-09 16:06:07 +0100

    use of credentials from request and taking api path from env vars

--- src/main/java/com/amt/mygarden/controllers/AuthController.java
+++ src/main/java/com/amt/mygarden/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package com.amt.mygarden.controllers;
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -7,11 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class AuthController {
+    @Value("${login-api.path}")
+    String loginAPIPath;
 
     @GetMapping(path = "/login")
     public String getLogin() {
@@ -20,11 +26,8 @@ public class AuthController {
 
     @PostMapping(path = "/login")
     public @ResponseBody
-    String authenticateUSer() {
-        String uri = "http://172.17.0.1:8082/auth/login";
-        // todo: add real credentials for test
-        String username = "xxx";
-        String password = "xxx";
+    String authenticateUSer(@RequestParam String username, @RequestParam String password) {
+        String uri = loginAPIPath + "/auth/login";
         String request = "{\"username\":\""+username+"\", \"password\":\""+password+"\"}";
 
         HttpHeaders headers = new HttpHeaders();
