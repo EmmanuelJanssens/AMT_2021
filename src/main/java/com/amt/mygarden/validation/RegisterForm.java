d7ff9dbf5eb0f990c23b364e268763e39f8aa618
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2021-12-12 13:28:56 +0100

    Model verification to register form

---
+++ src/main/java/com/amt/mygarden/validation/RegisterForm.java
@@ -0,0 +1,32 @@
+package com.amt.mygarden.validation;
+
+import jakarta.validation.constraints.NotNull;
+
+public class RegisterForm {
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "{\"username\":\""+username+"\", \"password\":\""+password+"\"}";
+    }
+}
